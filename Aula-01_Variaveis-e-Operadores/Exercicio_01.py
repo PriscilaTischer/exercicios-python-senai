365a94ba50f00a1855d94af5df95cfc4795c67ba
Priscila <pryscilaticher@hotmail.com>
2025-09-01 05:55:21 -0300

    Cria a estrutura de pastas e adiciona o exercício 1

---
+++ Aula-01_Variaveis-e-Operadores/Exercicio_01.py
@@ -0,0 +1,6 @@
+# Exercício 1: Solicitar dois números inteiros, multiplicar e mostrar o resultado.
+
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+resultado = num1 * num2
+print("O resultado da multiplicação é:", resultado)
